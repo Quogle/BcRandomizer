10b7ba3feff2e7f1c994d1c05e3cde63c9ac4e16
Quogle <yuggysloog9999@gmail.com>
2024-12-13 17:14:32 -0500

    executable jar should??? work
    Signed-off-by: Quogle <yuggysloog9999@gmail.com>

--- src/main/java/org/example/EnemyData.java
+++ src/main/java/org/example/EnemyData.java
@@ -2,29 +2,20 @@ package org.example;
 
 import com.opencsv.CSVReader;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Objects;
 
 
 public class EnemyData {
-    File enemy;
     String[][] enemyArray;
 
+    // Load resource as InputStream
     {
-        try {
-            enemy = new File(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("t_unit.csv")).toURI());
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    String csvFilePath = enemy.getPath();
-    {
-        try (
-                CSVReader reader = new CSVReader(new FileReader(csvFilePath))) {
+        try (InputStream inputStream = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResourceAsStream("t_unit.csv"));
+             CSVReader reader = new CSVReader(new InputStreamReader(inputStream))) {
+
             List<String[]> records = reader.readAll();
 
             enemyArray = new String[records.size()][];
@@ -32,17 +23,19 @@ public class EnemyData {
             for (int i = 0; i < records.size(); i++) {
                 enemyArray[i] = records.get(i);
             }
-        } catch (
-                IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public String[][] getEnemyArray() {
         return enemyArray;
     }
+
     public int getRowSize() {
         return enemyArray[0].length;
     }
+
     public int getColumnSize() {
         return enemyArray.length;
     }
