44a968ea3699e915979739ecb823b08f8f0d2118
tadpole1bc <the.tadpole.of.epicness@gmail.com>
2026-03-17 03:59:06 -0400

    made randomizer master file

---
+++ dev/randomizer/create_dl.py
@@ -0,0 +1,78 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+process:
+    parse config
+    create seed shit
+    create enemy/cat stat arrays
+    apply rebalances (metal behemoth early-exceptions)
+    wipe star/god
+    create map
+    force not trait/force trait fix (for exceptions and itf/cotc enemies)
+    set traits
+
+    ( reverse order if needed
+    rando ability
+    grant ability/stats
+    )
+    late exceptions
+    witch changes
+
+    
+    buff itf/cotc ex aliens
+    (do cat enemy id swap after ^)
+
+
+    
+    stage changes
+
+
+    ending shit:
+    get modded enemy/cat unit ids
+    add modded animations/stats
+    unitbuy?
+    add modded stages
+    youcant warning
+    special surprise
+
+    
+    
+
+
+
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
