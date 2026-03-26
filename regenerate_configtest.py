20dd689a2bb99a99f44f8353af79774444dba39c
tadpole1bc <the.tadpole.of.epicness@gmail.com>
2026-03-26 14:54:22 -0400

    fixed regen config test

--- regenerate_configtest.py
+++ regenerate_configtest.py
@@ -1,5 +1,9 @@
 import shutil
-from dev.randomizer.parse_config import *
+from dev.randomizer.parse_config import CAT_CONFIG
+from dev.randomizer.parse_config import ENEMY_CONFIG
+from dev.randomizer.parse_config import GAME_CONFIG
+from dev.randomizer.parse_config import CONFIGS
+from dev.randomizer.parse_config import DEFAULTS
 
 
 def wipe():
