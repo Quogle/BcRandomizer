54463fcb12b6c1ae1ef68bc58ce46559c4c79c95
Quogle <yuggysloog9999@gmail.com>
2026-09-06 11:33:21 -0400

    grid helper for checkboxes

--- src/bcr/gui/helpers/widgets.py
+++ src/bcr/gui/helpers/widgets.py
@@ -7,8 +7,10 @@ from PySide6.QtWidgets import (
     QDoubleSpinBox,
     QComboBox,
     QSlider,
-    QGridLayout
+    QGridLayout,
+    QCheckBox
 )
+
 
 
 class NoWheelSlider(QSlider):
@@ -31,52 +33,6 @@ class NoWheelDoubleSpinBox(QDoubleSpinBox):
 
     def wheelEvent(self, event):
         event.ignore()
-
-
-class WeightedList(QWidget):
-
-    def __init__(self, items=None):
-        super().__init__()
-
-        self.layout = QVBoxLayout(self)
-        self.layout.setContentsMargins(0, 0, 0, 0)
-        self.layout.setSpacing(6)
-
-        self.items = {}
-
-        if items:
-            for name, weight in items:
-                self.add_item(name, weight)
-
-    def add_item(self, name, weight=1):
-
-        row = QWidget()
-        row_layout = QHBoxLayout(row)
-
-        row_layout.setContentsMargins(0, 0, 0, 0)
-        row_layout.setSpacing(8)
-
-        label = QLabel(name)
-
-        weight_spin = NoWheelSpinBox()
-        weight_spin.setRange(0, 999)
-        weight_spin.setValue(weight)
-        weight_spin.setPrefix("Weight: ")
-
-        row_layout.addWidget(label)
-        row_layout.addStretch()
-        row_layout.addWidget(weight_spin)
-
-        self.layout.addWidget(row)
-
-        self.items[name] = weight_spin
-
-    def get_weights(self):
-
-        return {
-            name: spin.value()
-            for name, spin in self.items.items()
-        }
 
 
 class WeightedGrid(QWidget):
@@ -132,6 +88,49 @@ class WeightedGrid(QWidget):
         return {
             name: spin.value()
             for name, spin in self.items.items()
+        }
+
+
+class CheckboxGrid(QWidget):
+
+    def __init__(self, items=None, columns=2, names=None):
+        super().__init__()
+
+        self.layout = QGridLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setHorizontalSpacing(12)
+        self.layout.setVerticalSpacing(6)
+
+        self.columns = columns
+        self.names = names or {}
+        self.items = {}
+
+        if items:
+            for name, checked in items:
+                self.add_item(name, checked)
+
+    def add_item(self, name, checked=False):
+
+        checkbox = QCheckBox(
+            self.names.get(name, name)
+        )
+        checkbox.setChecked(checked)
+
+        index = len(self.items)
+
+        self.layout.addWidget(
+            checkbox,
+            index // self.columns,
+            index % self.columns
+        )
+
+        self.items[name] = checkbox
+
+    def get_values(self):
+
+        return {
+            name: checkbox.isChecked()
+            for name, checkbox in self.items.items()
         }
 
 
