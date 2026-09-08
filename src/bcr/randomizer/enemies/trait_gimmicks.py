68a443a8bdfd5a66a8f2c11f4029f09dd967fac0
tadpole1bc <the.tadpole.of.epicness@gmail.com>
2026-09-08 12:44:06 -0400

    added pillow to dependencies

--- src/bcr/randomizer/enemies/trait_gimmicks.py
+++ src/bcr/randomizer/enemies/trait_gimmicks.py
@@ -1,16 +1,17 @@
 import tadbcmc.data.enums.enemy as e
+from ...config.defaults import DEFAULT_CONFIG
 
 
 
 
 
 
-
-
-
-
-
-
+""" all these functions act on a single enemy and return it """
+def white_gimmick(stats,config=DEFAULT_CONFIG):
+    """ gives a white unit sage """
+    #the only thing is sage right?
+    do_sage = config["enemy"]["trait_gimmicks"]["sage"]
+    print(do_sage)
 
 
 
