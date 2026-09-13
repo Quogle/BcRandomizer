2caff81f7b696ddc58b4e9368c57c51ddadfc809
tadpole1bc <the.tadpole.of.epicness@gmail.com>
2026-09-13 11:34:44 -0400

    added the central module for the randomizer

---
+++ src/bcr/randomizer/randomize.py
@@ -0,0 +1,13 @@
+""" the central module for the game stats part of the randomizer\n
+everything that utilizes the players config to edit what ends up in download local must pass through here """
+from ..config.defaults import DEFAULT_CONFIG
+
+
+
+def randomize_according_to_config(config=DEFAULT_CONFIG,log=None):
+    """ anything regarding what ends up in download local MUST passs through this function
+    \n config and log should be passed here """
+    #I will add stuff here as I go
+
+
+
