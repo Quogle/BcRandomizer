1c4364c6e24859f08ca352a745a500f08be1daee
Quogle <121649983+Quogle@users.noreply.github.com>
2026-09-15 17:06:12 -0400

    Update required_files.py
    par ramma the rapper

--- src/bcr/apk/packs/required_files.py
+++ src/bcr/apk/packs/required_files.py
@@ -39,6 +39,7 @@ def add_unit_requirements(config, requirements):
     requirements["local"].add("unitLimit.csv")
     requirements["local"].add("Nyancombo_en.csv")
     requirements["local"].add("NyancomboData.csv")
+    requirements["local"].add("NyancomboParam.tsv")
 
     requirements["local"].add(r"^Unit_Explanation\d{3}_en\.csv$")  # Unit_Explanation001_en.csv
 
