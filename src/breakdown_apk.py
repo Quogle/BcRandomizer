858a03d66c5bfcb9126fd30d69183691875cb3da
tadpole1bc <the.tadpole.of.epicness@gmail.com>
2026-09-16 11:37:20 -0400

    working on making apk functions for testing

---
+++ src/breakdown_apk.py
@@ -0,0 +1,74 @@
+from bcr.apk.extract import extract_apk
+from bcr.apk.packs.decrypt import decrypt_packs
+from bcr.apk.server.downloader import download_server_files,process_server_files
+import os
+
+
+def _decrypt_apk(apk_path,output_dir):
+    """decrypts the apk at specified path to other specified path"""
+    extract_apk(apk_path,output_dir)
+
+
+def _decrypt_local_packs(dir_with_packs,output_dir,henry_style_output=False):
+    """ decrypts any packs found in specified directory to the specified directory
+    \n if henry style, will instead output each of the packs into their own directory """
+    #first get all the pack paths
+    all_in_pack_location = os.listdir(dir_with_packs)
+    all_packs = []
+    for file in all_in_pack_location:
+        if ".pack" in file:
+            all_packs.append(file)
+    print(all_packs)
+    #now decrypt them
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    if not henry_style_output:
+        for file in all_packs:
+            decrypt_packs(
+                pack_paths=os.path.join(dir_with_packs,file),
+                cc="en",
+                output_directory=output_dir,
+                )
+    else:
+        #for henry style output each one into a dir of its own name
+        for file in all_packs:
+            this_dir = os.path.join(output_dir,file.replace(".pack",""))
+            if not os.path.exists(this_dir):
+                os.makedirs(this_dir)
+            decrypt_packs(
+                pack_paths=os.path.join(dir_with_packs,file),
+                cc="en",
+                output_directory=this_dir,
+            )
+
+def _download_and_decrypt_server_files():
+    """ if henry it will output each server file into a dir of its own name """
+    apk_lib_path = lib_path = (
+                        decoded_directory
+                        / "lib"
+                        / "x86_64"
+                        / "libnative-lib.so"
+                    )
+
+
+
+download_server_files(
+                    lib_path=lib_path,
+                    tsv_paths=tsv_paths,
+                    country_code="en",
+                    output_directory=server_directory,
+                )
+decrypt_packs(
+                    pack_paths=server_pack_paths,
+                    cc="en",
+                    output_directory=decrypted_directory / "server",
+                )
+
+
+
+
+
+
+
+
+
